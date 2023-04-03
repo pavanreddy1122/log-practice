074aa12f9a71c69ffdd27c7b4e12d8ec6f254b33
pavanreddy1122 <pavanpav2003@@gmail.com>
2023-04-03 15:27:47 +0530

    log practice code added

---
+++ src/main/java/com/Logging/LogPractice/LogPracticeApplication.java
@@ -0,0 +1,20 @@
+package com.Logging.LogPractice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogPracticeApplication {
+
+	private static Logger log=LoggerFactory.getLogger(LogPracticeApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(LogPracticeApplication.class, args);
+		log.info("info started");
+		log.warn("warn");
+		log.error("error");
+		log.debug("debug");
+	}
+
+}
---
+++ src/test/java/com/Logging/LogPractice/LogPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Logging.LogPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
